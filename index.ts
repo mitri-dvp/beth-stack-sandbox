5cb4e29f4c01505829d959c0d59306ae31c1663c
mitri-dvp <mitri.dvp@gmail.com>
2023-09-09 22:23:48 -0500

    Installed HTML plugin for Elysia. Installed Typed-HTML.

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
 
-const app = new Elysia().get("/", () => "Hello World")
-    .listen(5000)
+const page = `<html lang="en">
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>`
+
+const app = new Elysia()
+  .use(html()).get("/", () => page)
+  .listen(5000)
 
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
