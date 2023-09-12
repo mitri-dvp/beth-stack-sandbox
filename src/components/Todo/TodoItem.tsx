e970fdc7e4ee3c5549b73e2f7a0bf0280ea43a2b
mitri-dvp <mitri.dvp@gmail.com>
2023-09-12 17:54:58 -0500

    Added web icons and improved TodoList+Items.

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -2,8 +2,12 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
   return (
     <div class="flex gap-4 text-4xl items-center">
       <p class="mr-auto">{todo.content}</p>
-      <input class="h-8 w-8" type="checkbox" checked={todo.completed} />
-      <button class="bg-red-500 text-white aspect-square p-4" />
+      <button class="w-8 h-8 rounded border border-gray-500 flex justify-center items-center active:text-gray-400 active:border-gray-400 transition-all">
+        {todo.completed ? <i class="bi bi-check"></i> : null}
+      </button>
+      <button class="bg-red-500 border border-red-500 text-white aspect-square w-8 h-8 text-base rounded hover:bg-red-600 transition-all">
+        <i class="bi bi-trash"></i>
+      </button>
     </div>
   );
 };
